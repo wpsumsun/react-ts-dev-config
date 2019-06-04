39a740b2b3a7f9cf489ede17644d55cbd78a2511
wangpeng <summer.wang@puman.com>
2019-06-05 00:59:33 +0800

    创建Icon组件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Button from './button'
+import Icon from './icon'
 
-ReactDOM.render(<Button></Button>, document.body)
+ReactDOM.render(<Icon name="wechat"/>, document.body)
 if (process.env.NODE_ENV !== 'production') {
    console.log('Looks like we are in development mode!');
 }
