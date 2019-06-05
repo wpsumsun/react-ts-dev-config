9ecdada8f9f029196470a8b44fca2cb1030ae1ae
wangpeng <summer.wang@puman.com>
2019-06-06 01:19:30 +0800

    Icon支持所有事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import './importAllIcons'
 import './icon.scss'
 
-interface IconProps {
-  name: String,
-  onClick?: React.MouseEventHandler<SVGElement>
+interface IconProps extends React.SVGAttributes<SVGElement>{
+  name: string
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
-  return (
-      <svg className='guluer-icon' onClick={ props.onClick }>
+    return (
+      <svg
+        className='guluer-icon'
+        { ...props }
+      >
         <use xlinkHref={ `#${props.name}` }/>
       </svg>
   )
--- lib/index.tsx
+++ lib/index.tsx
@@ -5,7 +5,7 @@ import Icon from './icon'
 const fn: React.MouseEventHandler = (e) => { console.log(e) }
 
 ReactDOM.render(<div>
-      <Icon name="alipay" onClick={ fn }/>
+      <Icon name="alipay" onClick={ fn } onMouseEnter={()=>{console.log('enter')}}/>
     </div>,
     document.querySelector('#root'))
 if (process.env.NODE_ENV !== 'production') {
