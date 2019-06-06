5b383f20c7c4874ff785657d126cd8dfaba3e062
wangpeng <summer.wang@puman.com>
2019-06-07 01:13:10 +0800

    测试click事件
    enzyme的使用

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,10 +1,18 @@
 import * as renderer from 'react-test-renderer'
 import React from 'react'
 import Icon from '../icon'
+import { mount } from 'enzyme'
 
 describe('Icon', () => {
   it('是个Icon', () => {
-    const json = renderer.create(<Icon name='alipay'/>).toJSON()
+    const json = renderer.create(<Icon name='alipay'/>).toJSON();
     expect(json).toMatchSnapshot()
+  });
+  it('触发click事件', () => {
+    const fn = jest.fn();
+    const fn2 = jest.fn();
+    const component = mount(<Icon name='alipay' onClick={ fn }/>);
+    component.simulate('click');
+    expect(fn).toBeCalled()
   })
-})
+});
