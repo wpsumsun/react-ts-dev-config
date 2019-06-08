8ac5cc6aec383e4574cc9036f3f4b7b267eec3e1
wangpeng <summer.wang@puman.com>
2019-06-08 18:47:32 +0800

    发布npm package

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,13 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Icon from './icon/icon'
-
-const fn: React.MouseEventHandler = (e) => { console.log(e) }
-
-ReactDOM.render(<div>
-      <Icon name="alipay" className='alipay' onClick={ fn } onMouseEnter={()=>{console.log('enter')}}/>
-    </div>,
-    document.querySelector('#root'))
-if (process.env.NODE_ENV !== 'production') {
-   console.log('Looks like we are in development mode!');
-}
+export { default as Icon } from './icon/icon';
