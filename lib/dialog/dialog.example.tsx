4c44263f524fcbb99510d499b1264e52f3a2ca04
wangpeng <summer.wang@puman.com>
2019-06-11 22:52:26 +0800

    dialog组件
    我写的是啥~~~~(>_<)~~~~

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,26 +1,50 @@
 import React, {useState} from 'react'
-import Dialog from './dialog'
+import Dialog, { confirm, alert, model } from './dialog'
 
 export default function () {
   const [x, setX] = useState(false)
   const handleClose: React.MouseEventHandler = () => {
     setX(false)
   }
+  const handleConfirm = () => {
+    confirm({
+      title: 'xx',
+      content: '您确定要删除吗？',
+      onCancel: (e) => { console.log(e) },
+      onOk:(e) => { console.log(e) }
+    })
+  }
+  const handleModel = () => {
+    model({
+      title: 'xx',
+      content: '您确定要删除吗？',
+      onCancel: (e) => { console.log(e) },
+      onOk:(e) => { console.log(e) }
+    })
+  }
+  const handleAlert = () => {
+    alert({
+      content: 'warning'
+    })
+  }
   return (
     <div>
+      <button onClick={handleConfirm}>confirm</button>
+      <button onClick={handleAlert}>alert</button>
+      <button onClick={handleModel}>model</button>
       <button style={{ position:'relative', zIndex:3 }} onClick={() => setX(!x)}>click</button>
       <Dialog
         visible={x}
         onCancel={handleClose}
         maskClosable
+        content={<h1>Hi1</h1>}
         footer={
           [
-            <button key="ok" onClick={() => {  }}>确认</button>,
+            <button key="ok" onClick={() => { setX(false) }}>确认</button>,
             <button key="cancel" onClick={() => { setX(false) }}>取消</button>
           ]
         }
         >
-        <h1>Hi</h1>
       </Dialog>
     </div>
   )
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment, ReactElement} from 'react'
+import React, {Fragment, ReactElement, ReactNode} from 'react'
 import ReactDOM from 'react-dom'
 import {completeClass} from '../utils/index'
 import Icon from '../icon/icon'
@@ -9,39 +9,44 @@ const cb = completeClassByPrefix
 
 interface Props {
   visible: boolean,
+  maskClosable?: boolean,
   okText?: string,
   cancelText?: string,
-  maskClosable?: boolean,
   footer?: Array<ReactElement>,
-  title?: Array<ReactElement>,
-  onCancel: React.MouseEventHandler
+  title?: String,
+  onOk?: React.MouseEventHandler,
+  onCancel?: React.MouseEventHandler,
+  content?: string | ReactNode
 }
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
   const handleCancel: React.MouseEventHandler = (e) => {
-    props.onCancel(e)
+    props.onCancel && props.onCancel(e)
+  }
+  const handleOk: React.MouseEventHandler = (e) => {
+    props.onOk && props.onOk(e)
   }
   const onClickMask: React.MouseEventHandler = (e) => {
     if (props.maskClosable) {
-      props.onCancel(e)
+      props.onCancel && props.onCancel(e)
     }
   }
   const dialog = props.visible ?
     <Fragment>
       <div className={cb()}>
         <header className={cb('header')}>
-          { props.title ? props.title : '提示' }
+          {props.title ? props.title : '提示'}
           <Icon name="close" onClick={handleCancel}/>
         </header>
-        <main className={cb('main')}>{ props.children }</main>
+        <main className={cb('main')}>{props.content ? props.content : props.children}</main>
         <footer className={cb('footer')}>
           {
             props.footer ?
-              props.footer.map((button) => React.cloneElement(button)) :
-              <Fragment>
-                <button>{ props.okText ? props.okText : '' }</button>
-                <button>{ props.cancelText ? props.cancelText : '' }</button>
-              </Fragment>
+            props.footer.map((button) => React.cloneElement(button)) :
+            <Fragment>
+              <button onClick={ handleOk }>{props.okText ? props.okText : '确定'}</button>
+              <button onClick={ handleCancel }>{props.cancelText ? props.cancelText : '取消'}</button>
+            </Fragment>
           }
         </footer>
       </div>
@@ -52,7 +57,97 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
     ReactDOM.createPortal(dialog, document.body)
   )
 }
-export function alert() {
-  
+
+interface ModelProps {
+  maskClosable?: boolean,
+  okText?: string,
+  cancelText?: string,
+  footer?: Array<ReactElement>,
+  title?: String,
+  onOk?: React.MouseEventHandler,
+  onCancel?: React.MouseEventHandler,
+  content?: string | ReactNode
 }
+
+export const confirm = (props: ModelProps) => {
+  const { onOk, onCancel, ...restProps } = props
+  const onClose: React.MouseEventHandler = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div)
+    ReactDOM.unmountComponentAtNode(div)
+    div.remove()
+  }
+  const handleOk: React.MouseEventHandler = (e) => {
+    onOk && onOk(e)
+    onClose(e)
+  }
+  const handleCancel: React.MouseEventHandler = (e) => {
+    onCancel && onCancel(e)
+    onClose(e)
+  }
+  const component = <Dialog
+    { ...restProps }
+    onOk={ handleOk }
+    onCancel={ handleCancel }
+    visible={true}
+  />
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  ReactDOM.render(component, div)
+}
+
+export const model = (props: ModelProps) => {
+  const { onOk, onCancel, ...restProps } = props
+  const onClose: React.MouseEventHandler = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div)
+    ReactDOM.unmountComponentAtNode(div)
+    div.remove()
+  }
+  const handleOk: React.MouseEventHandler = (e) => {
+    onOk && onOk(e)
+    onClose(e)
+  }
+  const handleCancel: React.MouseEventHandler = (e) => {
+    onCancel && onCancel(e)
+    onClose(e)
+  }
+  const component = <Dialog
+    { ...restProps }
+    onOk={ handleOk }
+    onCancel={ handleCancel }
+    visible={true}
+  />
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  ReactDOM.render(component, div)
+}
+
+export const alert = (props: ModelProps) => {
+  const { onOk, onCancel,...restProps } = props
+  const onClose: React.MouseEventHandler = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div)
+    ReactDOM.unmountComponentAtNode(div)
+    div.remove()
+  }
+  const handleOk: React.MouseEventHandler = (e) => {
+    onOk && onOk(e)
+    onClose(e)
+  }
+  const handleCancel: React.MouseEventHandler = (e) => {
+    onCancel && onCancel(e)
+    onClose(e)
+  }
+  const component = <Dialog
+    { ...restProps }
+    onOk={ handleOk }
+    onCancel={ handleCancel }
+    footer={
+      [ <button key="ok" onClick={onClose}>确认</button> ]
+    }
+    visible={true}
+  />
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  ReactDOM.render(component, div)
+}
+
 export default Dialog
